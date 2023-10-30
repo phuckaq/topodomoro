9aa31beccc0c2b8bbc75cefb5e2fc6e1735c1d76
phuckaq <phuc.dev.vn@gmail.com>
2023-10-30 19:35:43 +0700

    first commit

---
+++ src/main/java/phuc/topomodoro/TopomodoroApplication.java
@@ -0,0 +1,13 @@
+package phuc.topomodoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopomodoroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopomodoroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/phuc/topomodoro/TopomodoroApplicationTests.java
@@ -0,0 +1,13 @@
+package phuc.topomodoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopomodoroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
